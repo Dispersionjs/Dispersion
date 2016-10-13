171b8e721acec3fa2f2ff56471f9c982bb272489
jrrubalc <jason.ray.rubalcaba@gmail.com>
2016-10-12 20:32:07 -0700

    added cards for publishing

--- app/assets/js/dispersion.js
+++ app/assets/js/dispersion.js
@@ -48,6 +48,7 @@ const username = require('username');
                 "url": "https://ipfs.io/ipfs/" + hashArray[1]
               }
             }
+            console.log("Arr ",hashArray, "HashObj:", hashObj);
             //store in local storage
           storage.set(hashArray[1], hashObj[hashArray[1]], function(error) {
             if (error) throw error;
@@ -91,7 +92,7 @@ const username = require('username');
       exec(publishIt, function(error, stdout, stderr) {
         console.log(stdout, hash);
         let hashed = `http://gateway.ipfs.io/ipns/${stdout.split(' ')[2].slice(0, -1)}`
-        $('#hashlink').text(hashed);
+        //$('#hashlink').text(hashed);
         if (error !== null) {
           console.log('exec error: ' + error);
         }
