090fbc235bb82f6a538086c35991e204c5a48972
Jamey Stiling <jastiling@gmail.com>
2016-10-20 15:56:42 -0700

    unpin is now wrapped in promise

--- app/components/directives/daemon-load-directive.js
+++ app/components/directives/daemon-load-directive.js
@@ -1,11 +1,9 @@
-angular.module('daemonDirective', ['MainController'])
+angular.module('daemonDirective', [])
 
 .directive('daemonLoad', function() {
  return {
    restrict: 'E',
    transclude: true,
-   controller: 'MainController',
-   controllerAs: 'mainCtrl',
    scope: false,
    templateUrl: 'components/partials/daemon-load.html',
    replace: true
--- app/components/factories/fileFactory.js
+++ app/components/factories/fileFactory.js
@@ -1,85 +1,85 @@
 var module = angular
-  .module('FileFactory', [])
+    .module('FileFactory', [])
 
-module.service('FileFactory', ['$q','$timeout','IpfsService', fileFactory]);
+module.service('FileFactory', ['$q', '$timeout', 'IpfsService', fileFactory]);
 
-function fileFactory($q,$timeout,IpfsService) {
-  let fileData = [];
+function fileFactory($q, $timeout, IpfsService) {
+    let fileData = [];
 
-  function addHash() {
-    dialog.showOpenDialog({ properties: ['openFile', 'openDirectory', 'multiSelections'] }, function (addFiles) {
-      IpfsService.addFile(addFiles[0]);
-      $timeout(() => {
-        loadFilesFromStorage()
-      }, 1000)
-    });
-  }
-
-  function loadFilesFromStorage($scope) {
-    storage.keys(function (error, keys) {
-      if (error) throw error;
-      var promiseArr = [];
-      var fileArray = [];
-
-      //make promise array
-      keys.forEach((key, index, array) => {
-        promiseArr.push(
-          $q((resolve, reject) => {
-            storage.get(key, (error, data) => {
-              if (/Qm/.test(key)) {
-                fileArray.push({
-                  [key]: data
-                })
-              }
-              resolve();
-            })
-          }))
-      })
-
-      $q.all(promiseArr).then(() => {
-        console.log("All data from LOCAL STORAGE", fileArray)
-        fileData.length = 0;
-        fileData.push.apply(fileData, fileget(fileArray))
-      })
-    })
-  }
-
-  function fileget(fileArray) {
-    let arr = [];
-    let type;
-    fileArray.forEach(function (item, index) {
-      //finds file type
-      type = testFileType(item);
-      arr.push({
-        item: item[Object.keys(item)].file,
-        time: item[Object.keys(item)].time,
-        url: item[Object.keys(item)].url,
-        fileType: type,
-        hash: Object.keys(item)[0],
-        files: item[Object.keys(item)].files,
-      })
-    })
-    return arr;
-  };
-
-  function testFileType(item) {
-    let fileName = item[Object.keys(item)].file
-    console.log(item)
-    if (fileName.includes('.jpg') || fileName.includes('.png') || fileName.includes('.JPG') || fileName.includes('.PNG') || fileName.includes('.jpeg')) {
-      return 'image';
-    } else if (!fileName.includes('.')) {
-      return 'folder';
-    } else if (fileName.includes('.xl')) {
-      return 'excel';
-    } else if (fileName.includes('.pdf') || fileName.includes('.txt') || fileName.includes('.doc')) {
-      return 'doc';
+    function addHash() {
+        dialog.showOpenDialog({ properties: ['openFile', 'openDirectory', 'multiSelections'] }, function(addFiles) {
+            IpfsService.addFile(addFiles[0]);
+            $timeout(() => {
+                loadFilesFromStorage()
+            }, 1000)
+        });
     }
-  }
 
-  return {
-    addHash: addHash,
-    data: fileData,
-    init: loadFilesFromStorage,
-    loadFilesFromStorage: loadFilesFromStorage
-  }
+    function loadFilesFromStorage() {
+        console.log('inside files from storage')
+        storage.keys(function(error, keys) {
+            if (error) throw error;
+            var promiseArr = [];
+            var fileArray = [];
+
+            //make promise array
+            keys.forEach((key, index, array) => {
+                promiseArr.push(
+                    $q((resolve, reject) => {
+                        storage.get(key, (error, data) => {
+                            if (/Qm/.test(key)) {
+                                fileArray.push({
+                                    [key]: data
+                                })
+                            }
+                            resolve();
+                        })
+                    }))
+            })
+
+            $q.all(promiseArr).then(() => {
+                console.log("All data from LOCAL STORAGE", fileArray)
+                fileData.length = 0;
+                fileData.push.apply(fileData, fileget(fileArray))
+            })
+        })
+    }
+
+    function fileget(fileArray) {
+        let arr = [];
+        let type;
+        fileArray.forEach(function(item, index) {
+            //finds file type
+            type = testFileType(item);
+            arr.push({
+                item: item[Object.keys(item)].file,
+                time: item[Object.keys(item)].time,
+                url: item[Object.keys(item)].url,
+                fileType: type,
+                hash: Object.keys(item)[0],
+                files: item[Object.keys(item)].files,
+            })
+        })
+        return arr;
+    };
+
+    function testFileType(item) {
+        let fileName = item[Object.keys(item)].file
+        if (fileName.includes('.jpg') || fileName.includes('.png') || fileName.includes('.JPG') || fileName.includes('.PNG') || fileName.includes('.jpeg')) {
+            return 'image';
+        } else if (!fileName.includes('.')) {
+            return 'folder';
+        } else if (fileName.includes('.xl')) {
+            return 'excel';
+        } else if (fileName.includes('.pdf') || fileName.includes('.txt') || fileName.includes('.doc')) {
+            return 'doc';
+        }
+    }
+
+    return {
+        addHash: addHash,
+        data: fileData,
+        init: loadFilesFromStorage,
+        loadFilesFromStorage: loadFilesFromStorage
+    }
 }
