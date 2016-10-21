5ae87e2faf09c30010227aad584e43626ddfc9d2
yantonsoup <ygoldobin@gmail.com>
2016-10-21 16:47:03 -0700

    styling switched, publish to IPFS now works

--- app/components/controllers/publishController.js
+++ app/components/controllers/publishController.js
@@ -1,10 +1,13 @@
 angular
   .module('PublishController', [])
   //passing $scope and UserFactory as dependencies to controller
-  .controller('PublishController', ['PublishService', PublishController]);
+  .controller('PublishController', ['PublishService','IpfsService', PublishController]);
 
-function PublishController(PublishService) {
+function PublishController(PublishService, IpfsService) {
   PublishService.init().then(console.log('init called in publish controller'))
   const self = this;
   self.data = PublishService.data;
+
+  self.publishToIpfs = IpfsService.publish
+
 }
